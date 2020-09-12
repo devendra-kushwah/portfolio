75902e87e09ab80316b42493faf9802e8729dbcf
Devendra Kumar <devendra.kumar@lapr267.successivetech.com>
2020-09-12 17:08:09 +0530

    added work page

---
+++ src/Modules/Work/Work.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../Layout";
+
+const Work = () => {
+  return (
+    <Layout>
+      <div>
+        <h1 className="page-title">Work</h1>
+        <p>lorem ipsum</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Work;
